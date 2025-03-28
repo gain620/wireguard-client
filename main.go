7929f905d05d2d4dfe932785f19440a906424fae
gainchang <gainchang@naverz-corp.com>
2025-03-29 02:23:04 +0900

    add tunDevice read feature

--- main.go
+++ main.go
@@ -34,20 +34,23 @@ func main() {
 
 	log.Println("TUN interface created with automatic name:", tunDeviceName)
 
-	//packet := make([]byte, 2048)
-	//for {
-	//	n, err := tunDevice.Read(packet, 0)
-	//	if err != nil {
-	//		log.Printf("Error reading from TUN: %v", err)
-	//		continue
-	//	}
-	//
-	//	log.Printf("Received packet: % x", packet[:n])
-	//
-	//	// Echo the same packet back
-	//	_, err = tunDevice.Write(packet[:n], 0)
-	//	if err != nil {
-	//		log.Printf("Error writing to TUN: %v", err)
-	//	}
-	//}
+	packet := make([][]byte, 1)
+	packet[0] = make([]byte, 4096)
+	sizes := make([]int, 1)
+	for {
+		n, err := tunDevice.Read(packet, sizes, 4)
+		log.Printf("Read %d bytes from TUN", n)
+		if err != nil {
+			log.Printf("Error reading from TUN: %v", err)
+			continue
+		}
+
+		log.Printf("Received packet: % x", packet[0][:sizes[0]])
+
+		//// Echo the same packet back
+		//_, err = tunDevice.Write(packet, 0)
+		//if err != nil {
+		//	log.Printf("Error writing to TUN: %v", err)
+		//}
+	}
 }
